de2c8606bdaa02c7d6e4fa5743c15d9974e23bd8
markalston <mark@beernut.com>
2025-03-24 12:39:40 -0600

    adding my display to main

--- display/base_240x240.py
+++ display/base_240x240.py
@@ -6,7 +6,7 @@ PiFire Display Interface Library
 
  Description: 
    This is a base class for displays using 
- a 240Hx240W resolution.  Other display 
+ a 240Hx320W resolution.  Other display 
  libraries will inherit this base class 
  and add device specific features.
 
@@ -60,13 +60,9 @@ class DisplayBase:
 		180, 2 = 180 Degrees Rotation (Pimoroni Libraries, Luma.LCD Libraries)
 		270, 3 = 270 Degrees Rotation (Pimoroni Libraries, Luma.LCD Libraries)
 		'''
-		if self.rotation in [90, 270, 1, 3]:
-			self.WIDTH = 240
-			self.HEIGHT = 240
-		else:
-			self.WIDTH = 240
-			self.HEIGHT = 240
-
+		self.WIDTH = 240
+		self.HEIGHT = 240
+		
 		self.inc_pulse_color = True 
 		self.icon_color = 100
 		self.fan_rotation = 0
@@ -889,24 +885,14 @@ class DisplayBase:
 
 		if status_data['outpins']['fan']:
 			# F = Fan (Upper Left), position (10,10)
-			if self.WIDTH == 240:
-				self._draw_fan_icon(img, (10, 50))
-			else:
-				self._draw_fan_icon(img, (10, 10))
+			self._draw_fan_icon(img, (10, 10))
 
 		if status_data['outpins']['igniter']:
 			# I = Igniter(Center Right)
-			if self.WIDTH == 240:
-				self._draw_ignitor_icon(img, (self.WIDTH - 52, 170))
-			else:
-				self._draw_ignitor_icon(img, (self.WIDTH - 52, 60))
+			self._draw_ignitor_icon(img, (self.WIDTH - 52, 60))
 		
 		if status_data['outpins']['auger']:
-			# A = Auger (Center Left)
-			if self.WIDTH == 240:
-				self._draw_auger_icon(img, (10, 170))
-			else:
-				self._draw_auger_icon(img, (10, 60))
+			self._draw_auger_icon(img, (10, 60))
 
 		# Notification Indicator (Right)
 		show_notify_indicator = False
@@ -917,57 +903,41 @@ class DisplayBase:
 				notify_count += 1
 
 		if status_data['recipe_paused']:
-			if self.WIDTH == 240:
-				self._draw_pause_icon(img, (self.WIDTH - 52, 50))
-			else: 
-				self._draw_pause_icon(img, (self.WIDTH - 52, 10))
+			self._draw_pause_icon(img, (self.WIDTH - 52, 10))
 
 		elif status_data['recipe']:
-			if self.WIDTH == 240:
-				self._draw_recipe_icon(img, (self.WIDTH - 52, 50))
-			else: 
-				self._draw_recipe_icon(img, (self.WIDTH - 52, 10))
+			self._draw_recipe_icon(img, (self.WIDTH - 52, 10))
 
 		elif show_notify_indicator:
-			if self.WIDTH == 240:
-				self._draw_notify_icon(img, (self.WIDTH - 52, 50))
-			else: 
-				self._draw_notify_icon(img, (self.WIDTH - 52, 10))
+			self._draw_notify_icon(img, (self.WIDTH - 52, 10))
 
 			if notify_count > 1:
 				self._text_circle(draw, (self.WIDTH - 24, 40), (22, 22), str(notify_count), fg_color=(255, 255, 255), bg_color=(200, 0, 0))
 
 		# Smoke Plus Indicator
 		if status_data['s_plus'] and (status_data['mode'] == 'Smoke' or status_data['mode'] == 'Hold'):
-			if self.WIDTH == 240:
-				self._draw_splus_icon(img, (self.WIDTH - 52, 170))
-			else:
-				self._draw_splus_icon(img, (self.WIDTH - 52, 60))
+			self._draw_splus_icon(img, (self.WIDTH - 52, 60))
 
 		# Grill Hopper Level (Lower Center)
-		text = "Hopper:" + str(status_data['hopper_level']) + "%"
-		if status_data['hopper_level'] > 70:
-			hopper_color = (0, 255, 0)
-		elif status_data['hopper_level'] > 30:
-			hopper_color = (255, 150, 0)
-		else:
-			hopper_color = (255, 0, 0)
+		if status_data['hopper_level_enabled']:
+			text = "Hopper:" + str(status_data['hopper_level']) + "%"
+			if status_data['hopper_level'] > 70:
+				hopper_color = (0, 255, 0)
+			elif status_data['hopper_level'] > 30:
+				hopper_color = (255, 150, 0)
+			else:
+				hopper_color = (255, 0, 0)
 
-		label_canvas = self._draw_text(text, self.primary_font, 15, hopper_color, rect=True, outline_color=hopper_color, fill_color=(0,0,0))
-		if self.WIDTH == 240:
+			label_canvas = self._draw_text(text, self.primary_font, 15, hopper_color, rect=True, outline_color=hopper_color, fill_color=(0,0,0))
 			coords = self.WIDTH // 2 - (label_canvas.width // 2), self.HEIGHT - 28
-		else:
-			coords = self.WIDTH // 2 - (label_canvas.width // 2), (self.HEIGHT // 2) + 50
 
-		img.paste(label_canvas, coords, label_canvas)
+			img.paste(label_canvas, coords, label_canvas)
 
 		# Current Mode (Bottom Center)
 		text = status_data['mode']  # + ' Mode'
-		label_canvas = self._draw_text(text, self.primary_font, 32, (0,0,0), rect=True, outline_color=(3, 161, 252), fill_color=(255,255,255))
-		if self.WIDTH == 240:
-			coords = (self.WIDTH // 2 - (label_canvas.width // 2), 0)
-		else:
-			coords = (self.WIDTH // 2 - (label_canvas.width // 2), self.HEIGHT - 44)
+		label_canvas = self._draw_text(text, self.primary_font, 28, (0,0,0), rect=True, outline_color=(3, 161, 252), fill_color=(255,255,255))
+
+		coords = (self.WIDTH // 2 - (label_canvas.width // 2), self.HEIGHT - 48)
 		img.paste(label_canvas, coords, label_canvas)
 
 		# Draw Units Circle
@@ -982,16 +952,10 @@ class DisplayBase:
 			text_color = (0, 250, 0)
 
 			label_canvas = self._draw_text(text, self.primary_font, 15, text_color, rect=True, outline_color=text_color, fill_color=(0,0,0))
-			if self.WIDTH == 240:
-				if status_data['mode'] == 'Smoke':
-					coords = self.WIDTH // 2 - (label_canvas.width // 2), 60
-				else: 
-					coords = self.WIDTH // 2 - (label_canvas.width // 2), 210
-			else:
-				if status_data['mode'] == 'Smoke':
-					coords = self.WIDTH // 2 - (label_canvas.width // 2), 26
-				else: 
-					coords = self.WIDTH - label_canvas.width - 10, 10
+			if status_data['mode'] == 'Smoke':
+				coords = self.WIDTH // 2 - (label_canvas.width // 2), 26
+			else: 
+				coords = self.WIDTH - label_canvas.width - 10, 10
 
 			img.paste(label_canvas, coords, label_canvas)
 
@@ -1007,7 +971,7 @@ class DisplayBase:
 			countdown = int(duration - (time.time() - status_data['start_time'])) if int(duration - (time.time() - status_data['start_time'])) > 0 else 0
 			text = f'{countdown}s'
 			label_canvas = self._draw_text(text, self.primary_font, 26, (0,200,0), rect=True, outline_color=(0, 200, 0), fill_color=(0,0,0))
-			coords = (int((self.WIDTH // 2 )- (label_canvas.width // 2)), int((self.HEIGHT // 2) - 120))
+			coords = (int((self.WIDTH // 2 )- (label_canvas.width // 2)), 5)
 			img.paste(label_canvas, coords, label_canvas)
 
 		# Lid open detection timer display
@@ -1016,7 +980,7 @@ class DisplayBase:
 				duration = int(status_data['lid_open_endtime'] - time.time()) if int(status_data['lid_open_endtime'] - time.time()) > 0 else 0
 				text = f'Lid Pause {duration}s'
 				label_canvas = self._draw_text(text, self.primary_font, 18, (0,200,0), rect=True, outline_color=(0, 200, 0), fill_color=(0,0,0))
-				coords = (int((self.WIDTH // 2 )- (label_canvas.width // 2)), int((self.HEIGHT // 2) - 120))
+				coords = (int((self.WIDTH // 2 )- (label_canvas.width // 2)), 5)
 				img.paste(label_canvas, coords, label_canvas)
 
 		# Display Final Screen
@@ -1282,9 +1246,9 @@ class DisplayBase:
 
 		if self.menu['current']['mode'] == 'grill_hold_value':
 			# Grill Temperature (Large Centered)
-			font_point_size = 80 if self.WIDTH == 240 else 120 
+			font_point_size = 80
 			label_canvas = self._draw_text(str(self.menu['current']['option']), self.primary_font, font_point_size, (255,255,255))
-			label_origin = (int(self.WIDTH // 2 - label_canvas.width // 2), int(self.HEIGHT // 3 - label_canvas.height // 2)) if self.WIDTH == 240 else (int(self.WIDTH // 2 - label_canvas.width // 2 - 20), int(self.HEIGHT // 2.5 - label_canvas.height // 2))
+			label_origin = (int(self.WIDTH // 2 - label_canvas.width // 2), int(self.HEIGHT // 3 - label_canvas.height // 2))
 			img.paste(label_canvas, label_origin, label_canvas)
 
 			# Current Mode (Bottom Center)
@@ -1310,7 +1274,7 @@ class DisplayBase:
 					selected = item
 					break
 				index += 1
-			font_point_size = 80 if self.WIDTH == 240 else 120 
+			font_point_size = 80
 			icon_color = self.menu[self.menu['current']['mode']][selected].get('iconcolor', (255,255,255))  # Get color from menu item, default to white if not defined
 			text = self.menu[self.menu['current']['mode']][selected]['icon']
 			label_canvas = self._draw_text(text, 'static/font/FA-Free-Solid.otf', font_point_size, icon_color)
@@ -1319,7 +1283,7 @@ class DisplayBase:
 
 			# Draw a Plus Icon over the top of the Smoke Icon
 			if selected == 'SmokePlus':
-				font_point_size = 60 if self.WIDTH == 240 else 80
+				font_point_size = 60
 				text = '\uf067'
 				label_canvas = self._draw_text(text, 'static/font/FA-Free-Solid.otf', font_point_size, (0,0,0))
 				label_origin = (int(self.WIDTH // 2 - label_canvas.width // 2), int(self.HEIGHT // 2.5 - label_canvas.height // 2))
@@ -1348,7 +1312,7 @@ class DisplayBase:
 			down_color = (255, 255, 0)
 
 		# Up / Down Arrows (Middle Right)
-		font_point_size = 80 if self.WIDTH == 240 else 60 
+		font_point_size = 80
 		text = '\uf0de'  # FontAwesome Icon Sort (Up Arrow)
 		label_canvas = self._draw_text(text, 'static/font/FA-Free-Solid.otf', font_point_size, up_color)
 		label_origin = ((self.WIDTH - int((label_canvas.width // 2) ** 1.3)), int((self.HEIGHT // 2.5) - (label_canvas.height // 2) - 5))
