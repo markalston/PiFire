4776c9250a87d9a1a97f48df8193561438b25672
markalston <mark@beernut.com>
2025-03-30 08:57:29 -0600

    hiding toast when document ready.

--- static/js/dash_default.js
+++ static/js/dash_default.js
@@ -1072,6 +1072,9 @@ function dash_toast_success(message) {
 
 // Main
 $(document).ready(function(){
+	// Hide toast
+	$('#notifyToast').toast('hide');
+	
 	// Setup Listeners 
 	$('#reloadPage').click(function() {
 		// Reload page when server side changes detected. 
